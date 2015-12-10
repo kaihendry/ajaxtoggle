9a6eeb40652d706e10c152076d1f825a19c33eef
Kai Hendry <hendry@iki.fi>
2015-12-10 14:27:35 +0800

    vanillaJS

--- README.md
+++ README.md
@@ -5,11 +5,9 @@ All it does is touch/unlink $id into mute/ directory. Simply recording binary st
 
 I really really hate this code I have just written. Why?
 
-* Depends on Jquery
 * Took me several hours to write
 * I hate how the PHP and Javascript mirror each other
 * More than 50LOC for this is just too much imo
-* If two people using the interface, there is a real risk of accidentally toggling the button to opposite of intended state !!
 
 # BONUS
 
--- mute.php
+++ mute.php
@@ -1,18 +1,22 @@
 <?php
 
+header('Content-Type: application/json');
 $path = "mute";
 $id = $_POST["id"];
-echo "ID: " . $id;
-@mkdir($path);
 
 if (! empty($id) && is_numeric($id)) {
 	if (file_exists("$path/$id")) {
 		if (! unlink("$path/$id")) {
 			http_response_code(400);
+		} else {
+			echo json_encode(array("unmuted" => $id));
 		}
 	} else {
+		@mkdir($path);
 		if (! file_put_contents("$path/$id", $_SERVER['REMOTE_ADDR'])) {
 			http_response_code(400);
+		} else {
+			echo json_encode(array("muted" => $id));
 		}
 	}
 } else {
