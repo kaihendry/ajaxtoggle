22e9ec5137bf9fdd92e07d416e4c2608991c4bb8
Kai Hendry <hendry@iki.fi>
2015-12-10 14:30:33 +0800

    Link to source

--- index.php
+++ index.php
@@ -12,5 +12,7 @@
 <li><?=$id;?><button data-id=<?=$id;?>><?php echo (file_exists("mute/$id") ? 'Unmute' : 'Mute'); ?></button></li>
 <?php } ?>
 </ul>
+
+<p><a href="https://github.com/kaihendry/ajaxtoggle">Source code</a></p>
 </body>
 </html>
