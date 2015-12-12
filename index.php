ff5dba866a8e9143d539692d1e5eabb4c8bd9010
Kai Hendry <hendry@iki.fi>
2015-12-12 10:33:57 +0800

    Don't use toggles ;)

--- index.php
+++ index.php
@@ -8,10 +8,12 @@
 </head>
 <body>
 <ul>
-<?php foreach (array("1111", "2222") as $id) { ?>
+<?php foreach (array("1111", "2222", "3333") as $id) { ?>
 <li><?=$id;?><button data-id=<?=$id;?>><?php echo (file_exists("mute/$id") ? 'Unmute' : 'Mute'); ?></button></li>
 <?php } ?>
 </ul>
+
+<h3>WARNING: <a href=https://twitter.com/kaihendry/status/675502112080420864>Use checkboxes instead</a>!</h3>
 
 <p><a href="https://github.com/kaihendry/ajaxtoggle">Source code</a></p>
 </body>
